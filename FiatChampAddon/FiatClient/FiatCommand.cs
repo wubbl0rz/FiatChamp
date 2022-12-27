9f01972e6fd37939aa77bcd301beb466aa851269
AirBeeSee <57507903+AirBeeSee@users.noreply.github.com>
2022-12-27 13:48:48 +0100

    Update FiatCommand.cs

--- FiatChampAddon/FiatClient/FiatCommand.cs
+++ FiatChampAddon/FiatClient/FiatCommand.cs
@@ -5,13 +5,11 @@ public class FiatCommand
   public static readonly FiatCommand HBLF = new() { Message = "HBLF" };
   public static readonly FiatCommand CNOW = new() { Action = "ev/chargenow", Message = "CNOW" };
   public static readonly FiatCommand ROPRECOND = new() { Message = "ROPRECOND" };
-  public static readonly FiatCommand ROPRECOND_OFF = new() { Message = "ROPRECOND", IsDangerous = true };
+  public static readonly FiatCommand ROPRECOND_OFF = new() { Message = "ROPRECOND" };
   public static readonly FiatCommand ROTRUNKUNLOCK = new() { Message = "ROTRUNKUNLOCK" };
   public static readonly FiatCommand ROTRUNKLOCK = new() { Message = "ROTRUNKLOCK" };
-  public static readonly FiatCommand RDU = new() { Message = "RDU", IsDangerous = true };
-  public static readonly FiatCommand RDL = new() { Message = "RDL", IsDangerous = true };
-
-  public bool IsDangerous { get; set; }
+  public static readonly FiatCommand RDU = new() { Message = "RDU" };
+  public static readonly FiatCommand RDL = new() { Message = "RDL" };
   public required string Message { get; init; }
   public string Action { get; init; } = "remote";
 }
